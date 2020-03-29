68116585f3e23bb0d5e70e268d9f6493584117e3
João Marcos <joao.marcossantos@outlook.com>
2020-03-29 09:20:07 -0300

    calculo da mediana

--- VonFreyData2CloudModule/calculateNuploud.ino
+++ VonFreyData2CloudModule/calculateNuploud.ino
@@ -1,12 +1,13 @@
 void calculateNuploud(){
   int rodent; //contador para calculo para cada roedor
-  int aux; //auxiliar para ordenacao dos vetores de experimentos
-  float q1;
-  float median;
-  float q3;
 
   for(rodent = 0; rodent < (rodentsqtty - 1); rodent++){
+    int aux; //auxiliar para ordenacao dos vetores de experimentos
+    float q1; //primeiro quartil
+    float median; //mediana (segundo quartil)
+    float q3; //terceiro quartil
     int rodent_experimentqtty = 0; //quantidade de medidas feitas no roedor em analise
+    int empty_space = 0; //valores que restaram em -1
     
     lcd.clear();
     lcd.print("Processing...");
@@ -17,7 +18,12 @@ void calculateNuploud(){
       //conta quantos experimentos foram realizados, lembrando que vazio = -1
       //nao e tolerado valores negativos
       if(datacollected[rodent][i] > 0) rodent_experimentqtty++;
+      if(datacollected[rodent][i] == -1) empty_space++;
     }
+
+    //Se todos os valores do roedor n forem -1, então ele pula para o proximo
+    // a ser analisado (nao vai fazer o upload se roedor n não foi medido)
+    if(empty_space == xpqtty) continue;
 
     for (int atual = 0; atual < (rodent_experimentqtty - 1); atual++)
       /* ordenando o vetor para calculo da mediana e quartis, se o atual e maior
@@ -29,16 +35,32 @@ void calculateNuploud(){
           datacollected[rodent][proximo] = aux;
         }
 
-    if(rodent_experimentqtty % 2 == 0){
-      //Se a quantidade for par...
-      int empty_space = 0;
-      for(int empty_counter; empty_counter < xpqtty; empty_counter++)
-        if(datacollected[rodent][empty_counter] = -1) empty_space++;
+    if(rodent_experimentqtty%2 == 0){
+      /*Se a quantidade de experimentos feitos for par (sem resto na divisão por 2)
+       *a mediana pode ser calculada pelo ponto médio dos dois pontos centrais
+       *da amostra. É somada a quantidade de -1s que estao no inicio do vetor,
+       *assim como descontado um, ja que datacollected esta organizado em uma
+       *matriz, que por convencao comeca em 0
+      */
+      float a = datacollected[rodent][-1 + empty_space + rodent_experimentqtty/2];
+      float b = datacollected[rodent][-1 + empty_space + 1 + rodent_experimentqtty/2];
+      median = (a + b)/2;
+    }else{
+      /*
+       * Se for impar, a mediana e justamente o ponto central. Da mesma forma,
+       * e adicionada a quantidade de -1s e feito o desconto
+       */
+      int center = (((rodent_experimentqtty - 1)/2) + 1);
+      median = datacollected[rodent][empty_space - 1 + center];
     }
+
+    
+
+    
       
 
     for(int experiment = 0; experiment < rodent_experimentqtty; experiment++){
-
+      delay(1000);
     
     }
   } //for rodents
